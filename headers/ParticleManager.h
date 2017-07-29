c2bdd98895ab02de5124c7249e0707948547a2cd
May Drori <May Drori>
2017-07-29 19:12:31 +0300

    add particle manager

---
+++ headers/ParticleManager.h
@@ -0,0 +1,29 @@
+#ifndef __PARTICLE_MANAGER_H__
+#define __PARTICLE_MANAGER_H__
+
+#include <stack>
+#include "Particle.h"
+#include "Map.h"
+#include "Robot.h"
+
+#define PARTICLE_REMOVAL_THRESHOLD 			0.3
+#define PARTICLE_STRONG_SIGNAL_THRESHOLD	0.7
+#define PARTICLE_INIT_COUNT					100
+#define MAX_PARTICLES 						200
+#define PARTICLE_DUPLICATION_COUNT			10
+
+class ParticleManager
+{
+	private:
+		stack<Particle*> mParticles;
+
+	public:
+		ParticleManager(Map* map);
+		//virtual ~ParticleManager(); // nu bemet kilu nase po mashu
+
+		Particle* Update(Robot* robot, Map* map);
+		void ResampleParticles(Map* map);
+		void CreateRandomParticle(Map* map);
+};
+
+#endif
